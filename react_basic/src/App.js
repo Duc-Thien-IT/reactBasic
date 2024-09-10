cd5c3da181863bfeb33c21732bd3805ea85cd853
DucThien_Dev <ducthien0912.dev@gmail.com>
2024-09-10 09:21:31 +0700

    props

--- react_basic/src/App.js
+++ react_basic/src/App.js
@@ -1,26 +1,37 @@
 import './App.css';
+import { youtube } from "./Data";
 
 function App() {
   return (
-    <YoutubeItem>
-    </YoutubeItem>
+    <div className="youtube-list">
+      {youtube.map((item, index) => (
+        <YoutubeItem
+          key={item.id}
+          img={item.img || item.imgfooter}
+          imgfooter={item.imgfooter}
+          info={item.info}
+          author={item.author}
+        />
+      ))}
+    </div>
   );
 }
 
-function YoutubeItem (props){
+function YoutubeItem(props) {
   return (
-      <div class={props.ytb}>
-            <div class="youtube-image">
-                <img src={props.img} alt="" className="avt" />
-            </div>
-            <div class="youtube-footer">
-                <img src={props.imgfooter} alt="" className="avt-footer"/>
-            </div>
-            <div class="info">
-                <h3>{props.info}</h3>
-            </div>
+    <div className="youtube-item">
+      <div className="youtube-image">
+        <img src={props.img} alt="" className="avt" />
       </div>
-  )
+      <div className="youtube-footer">
+        <img src={props.imgfooter} alt="" className="avt-footer" />
+        <div className="info">
+          <h3>{props.info}</h3>
+          <h4>{props.author}</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ react_basic/src/Data.js
@@ -0,0 +1,25 @@
+export const youtube = [
+    {
+        id : 1,
+        info : "Clip funny",
+        author : "DThien",
+        img : "https://i.pinimg.com/236x/f1/6c/c7/f16cc7ec7235a12a99bf92505040bfdb.jpg",
+        imgfooter : "https://i.pinimg.com/236x/9a/3e/48/9a3e48043a2d4980ef887acf6f260116.jpg"
+    },
+
+    {
+        id : 2,
+        info : "Restream afterday",
+        author : "MyTien",
+        img : "https://i.pinimg.com/236x/f1/6c/c7/f16cc7ec7235a12a99bf92505040bfdb.jpg",
+        imgfooter : "https://i.pinimg.com/236x/9a/3e/48/9a3e48043a2d4980ef887acf6f260116.jpg"
+    },
+
+    {
+        id : 3,
+        info : "Video 3, News today in the world",
+        author : "DThien",
+        img : "https://i.pinimg.com/236x/f1/6c/c7/f16cc7ec7235a12a99bf92505040bfdb.jpg",
+        imgfooter : "https://i.pinimg.com/236x/9a/3e/48/9a3e48043a2d4980ef887acf6f260116.jpg"
+    }
+];
