33270385024bf2fb0212c9979d9740ed2feec46b
DucThien_Dev <ducthien0912.dev@gmail.com>
2024-09-11 10:13:24 +0700

    Bonus Game

--- react_basic/src/components/tictactoe/Cell.js
+++ react_basic/src/components/tictactoe/Cell.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Cell = (props) => {
+
     return (
         <div className="game-cell" onClick={props.onClick}>
             {props.value}
--- react_basic/src/components/tictactoe/Game.js
+++ react_basic/src/components/tictactoe/Game.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import "./GameStyle.css";
+import { calculateWinner } from '../../helper';
 
 const Game = () => {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const winner = calculateWinner(board);   
+    const handleClick = (index) => {
+        const boardCopy = [...Board];
+        if(winner || boardCopy[index]) return;
+        boardCopy[index] = xIsNext ? "X" : "O"  ;
+        setBoard(boardCopy);
+        setXIsNext(!xIsNext);    
+    };
     return (
         <div>
-            <Board></Board>
+            <Board cells = {board} onClick={handleClick} ></Board>
+            {winner ?  `Winner is ${xIsNext ? "X" : "O"}` :  ""};
         </div>
     );
 };
